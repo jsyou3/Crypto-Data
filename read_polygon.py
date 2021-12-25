2407330f592e68ef115abf1f2cfddd5952f157c6
franciscojavierarceo <arceofrancisco@gmail.com>
2021-12-25 17:24:18 -0500

    updated to get read inof

--- read_polygon.py
+++ read_polygon.py
@@ -4,8 +4,8 @@ import gspread
 import requests
 from oauth2client.service_account import ServiceAccountCredentials as SAC
 
-def get_polygon_data(headers:dict): 
-    url = 'https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2020-06-01/2020-06-17'
+def get_polygon_data(headers:dict, symbol: str): 
+    url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/2021-12-25/2021-12-25'
     r = requests.get(url, headers=headers)
     return r.json()
 
@@ -22,7 +22,15 @@ def authorize_gmail():
     client = gspread.authorize(creds)
     return client
 
-def get_tickers(client, sheet_name: str):
+def get_prices(headers: dict, tickers: list) -> dict:
+    pricedict = {}
+    for ticker in tickers:
+        url = f'https://api.polygon.io/v2/aggs/ticker/X:{ticker}USD/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120' 
+        r = requests.get(url, headers=headers)
+        pricedict[ticker] = r.json()
+    return pricedict
+
+def get_tickers(client, sheet_name: str) -> list:
     sheet = client.open(sheet_name).sheet1
     col = sheet.col_values(6)
     # elements after 🚀
@@ -34,13 +42,14 @@ def get_tickers(client, sheet_name: str):
     return tfinal
 
 def main():
-
     api_key = os.environ['POLYGON_API_KEY']
     headers = {
         'Authorization': f'Bearer {api_key}'
     }
     gclient = authorize_gmail()
-    print(get_tickers(gclient, 'Form Test'))
+    mytickers = get_tickers(gclient, 'Form Test')
+    myprices = get_prices(headers, mytickers)
+    print(myprices)
 
 if __name__ == '__main__':
     main()
